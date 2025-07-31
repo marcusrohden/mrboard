addb741e6c100e47784cc2b4a309c0fcd8469ccd
Marcus Rohden <marcus.rohden@gmail.com>
2025-07-31 23:34:43 +1000

    chore: removing unused windows, linux, mac files. Connected firebase, added mobx, modular and build_runner.

---
+++ lib/app_module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mrpostsboard/home_page.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.child('/', child: (context) => HomePage());
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Modular Page')),
+      body: Center(child: Text('This is the home page')),
+    );
+  }
+}
