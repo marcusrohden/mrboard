8a139db4ae14bc472e48795cba2bfef5a6a17ad8
Marcus Rohden <marcus.rohden@gmail.com>
2025-08-01 16:57:01 +1000

    chore: adding mobx dependencies

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+/// The default number of days to look back for messages.
+const int daysAgo = 7;
+
+/// The default number of items to fetch per pagination request.
+const int limitPagination = 25;
